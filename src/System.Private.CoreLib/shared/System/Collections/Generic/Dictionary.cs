5d0401281ab96b99339641118d2e049196ec14cf
Ben Adams <thundercat@illyriad.co.uk>
2019-10-12 08:23:41 +0100

    Fix out of bounds

--- src/System.Private.CoreLib/shared/System/Collections/Generic/Dictionary.cs
+++ src/System.Private.CoreLib/shared/System/Collections/Generic/Dictionary.cs
@@ -358,7 +358,11 @@ namespace System.Collections.Generic
                         {
                             // Should be a while loop https://github.com/dotnet/coreclr/issues/15476
                             // Test in if to drop range check for following array access
-                            if ((uint)i >= (uint)entries.Length || (entries[i].hashCode == hashCode && EqualityComparer<TKey>.Default.Equals(entries[i].key, key)))
+                            if ((uint)i >= (uint)entries.Length)
+                            {
+                                break; // Not found
+                            }
+                            else if (entries[i].hashCode == hashCode && EqualityComparer<TKey>.Default.Equals(entries[i].key, key))
                             {
                                 value = ref entries[i].value;
                                 break;
@@ -384,7 +388,11 @@ namespace System.Collections.Generic
                         {
                             // Should be a while loop https://github.com/dotnet/coreclr/issues/15476
                             // Test in if to drop range check for following array access
-                            if ((uint)i >= (uint)entries.Length || (entries[i].hashCode == hashCode && defaultComparer.Equals(entries[i].key, key)))
+                            if ((uint)i >= (uint)entries.Length)
+                            {
+                                break; // Not found
+                            }
+                            else if (entries[i].hashCode == hashCode && defaultComparer.Equals(entries[i].key, key))
                             {
                                 value = ref entries[i].value;
                                 break;
@@ -410,8 +418,11 @@ namespace System.Collections.Generic
                     {
                         // Should be a while loop https://github.com/dotnet/coreclr/issues/15476
                         // Test in if to drop range check for following array access
-                        if ((uint)i >= (uint)entries.Length ||
-                            (entries[i].hashCode == hashCode && comparer.Equals(entries[i].key, key)))
+                        if ((uint)i >= (uint)entries.Length)
+                        {
+                            break; // Not found
+                        }
+                        else if (entries[i].hashCode == hashCode && comparer.Equals(entries[i].key, key))
                         {
                             value = ref entries[i].value;
                             break;
